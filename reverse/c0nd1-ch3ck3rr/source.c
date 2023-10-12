be8ae8f99a3649afcc2646cb57d3708e796404c0
kamch4tskiy <kamchatskiymma@gmail.com>
2023-10-12 18:20:51 +0300

    add c0nd1-ch3ck3rr

---
+++ reverse/c0nd1-ch3ck3rr/source.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char input[256];
+  scanf("%255s", input);
+
+  unsigned short int i = 0;
+  if (input[i] != 'C') {
+    printf("nope\n");
+    return 0;
+  }
+  i++;
+  if (input[i] != 'T') {
+    printf("nope\n");
+    return 0;
+  }
+  i++;
+  if (input[i] != 'F') {
+    printf("nope\n");
+    return 0;
+  }
+  i++;
+  if (input[i] != 'l') {
+    printf("nope\n");
+    return 0;
+  }
+  i++;
+  if (input[i] != 'y') {
+    printf("nope\n");
+    return 0;
+  }
+  i++;
+  if (input[i] != 'c') {
+    printf("nope\n");
+    return 0;
+  }
+  i++;
+  if (input[i] != '{') {
+    printf("nope\n");
+    return 0;
+  }
+  i++;
+  if (input[i] != 's') {
+    printf("nope\n");
+    return 0;
+  }
+  i++;
+  if (input[i] != '0') {
+    printf("nope\n");
+    return 0;
+  }
+  i++;
+  if (input[i] != '_') {
+    printf("nope\n");
+    return 0;
+  }
+  i++;
+  if (input[i] != 'm') {
+    printf("nope\n");
+    return 0;
+  }
+  i++;
+  if (input[i] != '4') {
+    printf("nope\n");
+    return 0;
+  }
+  i++;
+  if (input[i] != 'n') {
+    printf("nope\n");
+    return 0;
+  }
+  i++;
+  if (input[i] != 'y') {
+    printf("nope\n");
+    return 0;
+  }
+  i++;
+  if (input[i] != '_') {
+    printf("nope\n");
+    return 0;
+  }
+  i++;
+  if (input[i] != 'c') {
+    printf("nope\n");
+    return 0;
+  }
+  i++;
+  if (input[i] != '0') {
+    printf("nope\n");
+    return 0;
+  }
+  i++;
+  if (input[i] != 'n') {
+    printf("nope\n");
+    return 0;
+  }
+  i++;
+  if (input[i] != 'd') {
+    printf("nope\n");
+    return 0;
+  }
+  i++;
+  if (input[i] != '1') {
+    printf("nope\n");
+    return 0;
+  }
+  i++;
+  if (input[i] != 't') {
+    printf("nope\n");
+    return 0;
+  }
+  i++;
+  if (input[i] != '1') {
+    printf("nope\n");
+    return 0;
+  }
+  i++;
+  if (input[i] != '0') {
+    printf("nope\n");
+    return 0;
+  }
+  i++;
+  if (input[i] != 'n') {
+    printf("nope\n");
+    return 0;
+  }
+  i++;
+  if (input[i] != '$') {
+    printf("nope\n");
+    return 0;
+  }
+  i++;
+  if (input[i] != '}') {
+    printf("nope\n");
+    return 0;
+  }
+
+  printf("Y3p, 1t'$ c0rr3ct\n");
+
+  return 0;
+}
--- reverse/e4sy-stringi/source.c
+++ reverse/e4sy-stringi/source.c
@@ -2,14 +2,14 @@
 #include <string.h>
 
 int main() {
-    char input[256];
-    scanf("%255s", input);
+  char input[256];
+  scanf("%255s", input);
 
-    if (strcmp(input, "CTFlyc{b4by_r3v3rs3}") == 0) {
-        printf("y0u'r3 g0ddamn right");
-    } else {
-        printf("aboba.");
-    }
-    
-    return 0;
+  if (strcmp(input, "CTFlyc{b4by_r3v3rs3}") == 0) {
+    printf("y0u'r3 g0ddamn right");
+  } else {
+    printf("aboba.");
+  }
+
+  return 0;
 }
